2228bfa8c00665149811c24c93a3901b0ec9206c
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-09-15 11:09:18 +0530

    Find Frequency of Words

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -2,6 +2,23 @@ package com.bridgelabz;
 
 public class HashTableMain {
     public static void main(String[] args) {
-        System.out.println("Frequency of Words\n\n");
+        System.out.println("Frequency of Words\n");
+
+        String paragraph = "to be or not to be";
+        String[] splitArray = paragraph.split(" ");
+
+        HashTable<String, Integer> hashTable = new HashTable<>();
+
+        for (String word: splitArray) {
+            if(hashTable.containsKey(word)){
+
+                int count =  hashTable.get(word);
+                hashTable.replace(word,count + 1);
+            }
+            else{
+                hashTable.put(word,1);
+            }
+        }
+        hashTable.print();
     }
 }
